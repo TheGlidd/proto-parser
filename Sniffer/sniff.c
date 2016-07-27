617e515ff7a7d80b598d7939df2ebd3b6a9ae454
TheGlidd <reveym@yahoo.com>
2016-07-26 22:31:55 -0400

    starting to look at some 'frames'

---
+++ Sniffer/sniff.c
@@ -0,0 +1,34 @@
+/*
+compile with gcc sniff.c -lpcap
+
+*/
+
+#include <stdio.h>
+#include <pcap.h>
+
+int main(int argc, char *argv[]) {
+
+    char *dev = "eth0";
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle;
+    const u_char *packet;
+    struct pcap_pkthdr header;
+    
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL ) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+        return 2;
+    } 
+    
+    if (pcap_datalink(handle) != DLT_EN10MB) {
+        fprintf(stderr, "Device %s doesn't provide Ethernet headers\n", dev);
+        return 2;
+    }
+    
+    packet = pcap_next(handle, &header);
+    printf("Jacked length %d\n", header.len);
+    pcap_close(handle);
+
+    return 0;
+
+}
